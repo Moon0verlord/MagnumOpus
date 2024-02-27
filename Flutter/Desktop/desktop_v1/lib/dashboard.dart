cdf1099699a1b2a5db8d8d04ef0ed4f0d2ca5560
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2024-02-27 20:04:42 +0100

    Added navigation bar to dashboard

--- Flutter/Desktop/desktop_v1/lib/dashboard.dart
+++ Flutter/Desktop/desktop_v1/lib/dashboard.dart
@@ -9,17 +9,52 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Center(
-        child: Text(
-          'Dashboard Screen Placeholder',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Row(
+        children: [
+          SafeArea(
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return NavigationRail(
+                  extended: true,
+                  destinations: const [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.charging_station),
+                      label: Text('Charging Stations'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.account_circle),
+                      label: Text('Profile'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.battery_charging_full),
+                      label: Text('Charge Status'),
+                    ),
+                  ],
+                  selectedIndex: 0,
+                  onDestinationSelected: (value) {
+                    print('selected: $value');
+                  },
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: const Center(
+                child: Text(
+                  'Dashboard Screen Placeholder',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
-
