740026206339bed0acc551cf923e4a09ddd10ccc
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2024-04-15 21:59:40 +0200

    Added Desktop login page. frontend only

--- Svelte/my-app/src/routes/+page.svelte
+++ Svelte/my-app/src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+
+  import type {PageData} from './$types';
+  import {mobile} from './mobile/mobile';
+
     let isLoading = false;
     let email = '';
     let password = '';
@@ -7,12 +11,14 @@
     let alertMessageDisplay = false;
     let alertMessage = ''
 
+    $: isMobile = $mobile;
+
+
     async function handleLoginKey(event: KeyboardEvent) {
         if (event.key === 'Enter') {
             if (email === '' || password === '') {
                 return;
             }
-
             await handleLogin();
         }
     }
@@ -85,46 +91,94 @@
     </div>
 {/if}
 
+
+{#if isMobile}
 <div class="flex items-center justify-center h-screen">
-    <div class="card bg-base-100 shadow-xl">
-        <div class="card-body">
-            <h2 class="card-title">Login</h2>
-            <label class="input input-bordered {isEmailError ? 'input-error animate-pulse' : ''} flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                     class="w-4 h-4 opacity-70">
-                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
-                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
-                </svg>
-                <input type="text" class="grow" placeholder="Email" bind:value={email} on:keydown={handleLoginKey}
-                       on:input={handleInput}/>
-            </label>
-            <label class="input input-bordered {isPasswordError ? 'input-error animate-pulse' : ''} flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                     class="w-4 h-4 opacity-70">
-                    <path fill-rule="evenodd"
-                          d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                          clip-rule="evenodd"/>
-                </svg>
-                <input type="password" class="grow" placeholder="Password" bind:value={password}
-                       on:keydown={handleLoginKey} on:input={handleInput}/>
-            </label>
-            <button class="btn btn-block" on:click={handleLogin}>
-                {#if isLoading}
-                    <span class="loading loading-spinner"></span>
-                {:else}
-                    Login
-                {/if}
-            </button>
-            <div class="label">
-                <span class="label-text-alt">
-                    <a href="/register" class="link link-hover">Register</a>
-                </span>
-                <span class="label-text-alt"><a href="/forgotpassword" class="link link-hover">Forgot Password</a></span>
-            </div>
-            <div class="divider">or</div>
-            <button class="btn btn-block">
-                Login with Okta
-            </button>
-        </div>
-    </div>
+  <div class="card bg-base-100 shadow-xl">
+      <div class="card-body">
+          <h2 class="card-title">Login</h2>
+          <label class="input input-bordered {isEmailError ? 'input-error animate-pulse' : ''} flex items-center gap-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                   class="w-4 h-4 opacity-70">
+                  <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+                  <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+              </svg>
+              <input type="text" class="grow" placeholder="Email" bind:value={email} on:keydown={handleLoginKey}
+                     on:input={handleInput}/>
+          </label>
+          <label class="input input-bordered {isPasswordError ? 'input-error animate-pulse' : ''} flex items-center gap-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                   class="w-4 h-4 opacity-70">
+                  <path fill-rule="evenodd"
+                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                        clip-rule="evenodd"/>
+              </svg>
+              <input type="password" class="grow" placeholder="Password" bind:value={password}
+                     on:keydown={handleLoginKey} on:input={handleInput}/>
+          </label>
+          <button class="btn btn-block" on:click={handleLogin}>
+              {#if isLoading}
+                  <span class="loading loading-spinner"></span>
+              {:else}
+                  Login
+              {/if}
+          </button>
+          <div class="label">
+              <span class="label-text-alt">
+                  <a href="/register" class="link link-hover">Register</a>
+              </span>
+              <span class="label-text-alt"><a href="/forgotpassword" class="link link-hover">Forgot Password</a></span>
+          </div>
+          <div class="divider">or</div>
+          <button class="btn btn-block">
+              Login with Okta
+          </button>
+      </div>
+  </div>
 </div>
+
+{:else}
+
+  <div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[100vh]">
+    <!-- Blank section with logo and quote on the left -->
+    <div class="flex flex-col justify-between p-12 bg-base-200 text-base-content">
+      <div>
+        <img src="src/lib/assets/logo.svg" alt="Acme Inc Logo" class="w-24 h-24 mb-4" />
+        <h2 class="text-2xl font-bold mb-3">Acme Inc</h2>
+        <blockquote>
+          "Drip or drown"
+        </blockquote>
+        <cite>Mozart</cite>
+      </div>
+    </div>
+  
+    <!-- Login section on the right -->
+    <div class="flex items-center justify-center bg-base-100 py-12 lg:min-h-[600px] xl:min-h-[100vh]">
+      <div class="w-full max-w-md">
+        <div class="mb-6">
+          <h1 class="text-3xl font-bold text-center mb-4">Login</h1>
+          <form on:submit|preventDefault={handleLogin}>
+            <div class="mb-4">
+              <label for="email" class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input id="email" type="email" placeholder="name@example.com" class="input input-bordered w-full" required on:keydown={handleLoginKey} bind:value={email} />
+            </div>
+            <div class="mb-6">
+              <label for="password" class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input id="password" type="password" class="input input-bordered w-full" required on:keydown={handleLoginKey} bind:value={password} />
+              <a href="##" class="label-text-alt link link-hover">Forgot Password?</a>
+            </div>
+            <button type="submit" class="btn btn-primary w-full">Login</button>
+          </form>
+        </div>
+        <p class="text-center text-sm">
+          Don't have an account?
+          <a href="##" class="link link-hover">Sign up</a>
+        </p>
+      </div>
+    </div>
+  </div>
+  {/if}
