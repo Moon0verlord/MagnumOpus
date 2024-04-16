0024552c50edb092a33c7d5d309a40724a0fd58f
Rokimajo <robinvliegtvandaag@gmail.com>
2024-04-16 18:12:36 +0200

    Changed pagination to not fit the whole screen and instead dynamically render disabled ... buttons.

--- Svelte/my-app/src/routes/stations/+page.svelte
+++ Svelte/my-app/src/routes/stations/+page.svelte
@@ -24,6 +24,8 @@
         end = start + itemsPerPage;
         currentPageData = data.props.chargingPorts.slice(start, end);
     };
+    let paginationFlag = false;
+    let setFlag = () => paginationFlag = true;
 </script>
 
 
@@ -76,7 +78,11 @@
                     </div>
                     <div class="join flex justify-center">
                         {#each Array(Math.ceil(data.props.chargingPorts.length / itemsPerPage)) as _, i (i)}
-                            <button class="join-item btn" on:click={() => goToPage(i + 1)}>{i + 1}</button>
+                            {#if i === 0 || i === Math.ceil(data.props.chargingPorts.length / itemsPerPage) - 1 || i === currentPage || (i >= currentPage - 3 && i <= currentPage + 1)}
+                                <button class="join-item btn" on:click={() => goToPage(i + 1)}>{i + 1}</button>
+                            {:else if i === currentPage - 4 || i === currentPage + 2}
+                                <button class="join-item btn btn-disabled">...</button>
+                            {/if}
                         {/each}
                     </div>
                 </div>
