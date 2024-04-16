e921aea022b6503fa44802cd0092bbd1c689591a
Rokimajo <robinvliegtvandaag@gmail.com>
2024-04-16 17:26:05 +0200

    Added DB server loading for ports.

---
+++ Svelte/my-app/src/lib/server/db/dbComposables.ts
@@ -0,0 +1,6 @@
+﻿import {type Port, Ports} from "$lib/server/db/schema";
+import {db} from "$lib/server/db/db.server";
+
+export const GetAllPorts = async () : Promise<Port[]> => {
+    return await db.select().from(Ports).execute();
+};
