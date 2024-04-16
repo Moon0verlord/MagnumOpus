b138d133599450b573895db30d3d019542875831
Calvin <loncsime@gmail.com>
2024-04-16 17:48:29 +0200

    -MT fix for when Navbar is not present

--- Svelte/my-app/src/routes/+error.svelte
+++ Svelte/my-app/src/routes/+error.svelte
@@ -4,7 +4,7 @@
     <h1 class="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Page not found</h1>
     <p class="mt-6 text-base leading-7">Sorry, we couldn’t find the page you’re looking for.</p>
     <div class="mt-10 flex items-center justify-center gap-x-6">
-      <a href="/home" class="rounded-md bg-base-300/30 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-base-300/50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
+      <a href="/home" class="rounded-md bg-base-300/30 px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-base-300/50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
     </div>
   </div>
 </main>
