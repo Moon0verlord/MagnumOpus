31747b737b42274a045edef03205657696e5b513
Rokimajo <robinvliegtvandaag@gmail.com>
2024-04-17 10:07:19 +0200

    Added new ports test page to work with stations and collapsible ports, still WIP and iffy, however the proof of concept works.

--- Svelte/my-app/src/routes/test/+page.svelte
+++ Svelte/my-app/src/routes/test/+page.svelte
@@ -1,56 +1,89 @@
 <script lang="ts">
-    import {checkAndSendNotification} from '$lib/notification';
+    import {tick} from 'svelte';
+    import type {PageData} from './$types';
+    import {mobile} from '../mobile/mobile';
+    import charger from "$lib/assets/MaterialSymbolsEvCharger.svg";
+    import charge from "$lib/assets/SolarBatteryChargeBold.svg";
+    import lock from "$lib/assets/MaterialSymbolsLockOpenRight.svg";
 
-    let title = '';
-    let body = '';
+    $: isMobile = $mobile;
+    let selectedStationId: string = "";
+    export let data: PageData;
 
-    function sendNotificationExample() {
-        checkAndSendNotification(title, body);
-        title = '';
-        body = '';
-    }
+    let currentPage = 1; // Current page number
+    const itemsPerPage = 5; // Number of items per page
+
+    // Calculate start and end indices for slicing the data array
+    let start = (currentPage - 1) * itemsPerPage;
+    let end = start + itemsPerPage;
+
+    // Get the data for the current page
+    let currentPageData = data.props.stations.slice(start, end);
+
+    // Function to go to a specific page
+    const goToPage = (page: any) => {
+        currentPage = page;
+        start = (currentPage - 1) * itemsPerPage;
+        end = start + itemsPerPage;
+        currentPageData = data.props.stations.slice(start, end);
+    };
+    let paginationFlag = false;
+    let setFlag = () => paginationFlag = true;
 </script>
 
-<div class="flex justify-center items-center h-screen w-screen">
-    <div class="w-3/4">
-        <div class="carousel w-full">
-            <div id="item1" class="carousel-item w-full">
-                <div class="card w-full bg-base-200 mx-auto">
-                    <div class="card-body">
-                        <h2 class="card-title">Send notification</h2>
-                        <div>
-                            <label class="input input-bordered flex items-center gap-2">
-                                <input type="text" class="grow" placeholder="Title" bind:value={title}/>
-                            </label>
-                            <label class="input input-bordered flex items-center gap-2 mt-2.5">
-                                <input type="text" class="grow" placeholder="Body" bind:value={body}/>
-                            </label>
-                            <button class="btn mt-2.5 bg-base-300/100" on:click={sendNotificationExample}>Send
-                                Notification
-                            </button>
-                        </div>
+
+<!-- Ports -->
+{#if !isMobile}
+    <div class="flex items-center justify-center h-screen">
+        <div class="flex-grow flex w-full items-center h-screen">
+            <div class="card bg-base-100 shadow-xl mx-auto mt-2.5">
+                <div class=" w-auto card-body">
+                    <h2 class="card-title">Ports</h2>
+                    <div class="overflow-x-auto">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Station ID</th>
+                                <th>Power</th>
+                                <th>Amount of Ports</th>
+                                <th>Status</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {#each currentPageData as station}
+                                <tr>
+                                    <td>{station.stationId}</td>
+                                    <td>{station.maxPower}</td>
+                                    <td>{station.portIds?.split(",").length}</td>
+                                    <td class="badge badge-secondary">{station.overallStatus}</td>
+                                    <td>
+                                        <input type="checkbox" class="checkbox" checked={selectedStationId === station.stationId} on:change={() => selectedStationId = (selectedStationId === station.stationId ? "" : station.stationId)} />
+                                    </td>
+                                </tr>
+                                {#if selectedStationId === station.stationId}
+                                    {#each station.portIds.split(",") as port}
+                                        <tr>
+                                            <td class="badge badge-accent">Port: {port}</td>
+                                        </tr>
+                                    {/each}
+                                {/if}
+                            {/each}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="join flex justify-center">
+                        {#each Array(Math.ceil(data.props.stations.length / itemsPerPage)) as _, i (i)}
+                            {#if i === 0 || i === Math.ceil(data.props.stations.length / itemsPerPage) - 1 || i === currentPage || (i >= currentPage - 3 && i <= currentPage + 1)}
+                                <button class="{i + 1 === currentPage ? 'join-item btn bg-base-300' : 'join-item btn'}" on:click={() => goToPage(i + 1)}>{i + 1}</button>
+                            {:else if i === currentPage - 4 || i === currentPage + 2}
+                                <button class="join-item btn btn-disabled">...</button>
+                            {/if}
+                        {/each}
                     </div>
                 </div>
             </div>
-            <div id="item2" class="carousel-item w-full">
-                <div class="card w-full bg-base-200 mx-auto">
-                    <div class="card-body">
-                        <h2 class="card-title">2</h2>
-                    </div>
-                </div>
-            </div>
-            <div id="item3" class="carousel-item w-full">
-                <div class="card w-full bg-base-200 mx-auto">
-                    <div class="card-body">
-                        <h2 class="card-title">3</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="flex justify-center w-full py-2 gap-2">
-            <a href="#item1" class="btn btn-xs">1</a>
-            <a href="#item2" class="btn btn-xs">2</a>
-            <a href="#item3" class="btn btn-xs">3</a>
         </div>
     </div>
-</div>
+{:else}
+{/if}
