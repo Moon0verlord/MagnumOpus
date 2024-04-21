bcf10a98317284a9f42ab9fbe930c8216e6cdf2a
Rokimajo <robinvliegtvandaag@gmail.com>
2024-04-21 19:57:43 +0200

    added slack integration lol

---
+++ Svelte/my-app/src/routes/api/slack/+server.ts
@@ -0,0 +1,19 @@
+﻿import {SLACK_WEBHOOK} from "$env/static/private";
+
+// @ts-ignore
+export const POST = async ({ request }) => {
+    const body = await request.json();
+    const response = await fetch(SLACK_WEBHOOK, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            text: body.message,
+        }),
+    });
+    if (response.ok) {
+        return new Response(JSON.stringify({ message: "Success"}), { status: 201 });
+    }
+    return new Response(JSON.stringify({ message: "Failed"}), { status: 400 });
+}
--- Svelte/my-app/src/routes/home/+page.svelte
+++ Svelte/my-app/src/routes/home/+page.svelte
@@ -22,6 +22,20 @@
     let currentUserInfo: User | null;
     let unsubscribe: () => void;
     let pageData: any[] = [];
+    
+    async function SendSlackMsg() {
+        console.log('inside slack msg');
+        const response = await fetch('/api/slack', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                message: 'Quadringus was here.',
+            }),
+        });
+        console.log(response);
+    }
 
 
     async function getOktaUserInfo() {
@@ -557,6 +571,7 @@
                                     <h1 class="card-title text-5xl">Welcome Guest</h1>
                                 {/if}
                                 <p>So glad to see you! Let's get started with managing your session.</p>
+                                <button class="btn bg-base-300" on:click={() => SendSlackMsg()}>Send Slack Message</button>
                             </div>
                         </div>
                     </div>
