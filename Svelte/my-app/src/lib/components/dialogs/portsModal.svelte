46bc56fa3736653aef2647e39af4ba0d07783f9e
WarMartijn <1058740@hr.nl>
2024-05-27 21:01:18 +0200

    fixed users being able to use letters where you should use numbers

--- Svelte/my-app/src/lib/components/dialogs/portsModal.svelte
+++ Svelte/my-app/src/lib/components/dialogs/portsModal.svelte
@@ -88,10 +88,12 @@
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
                                 </svg>
                             </button>
-                 
+                            // The [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none 
+                            [&::-webkit-inner-spin-button]:appearance-none in here is used to remove the default arrows in the input field
                             <input type="number" id="quantity-input" 
                                    
                                    data-input-counter aria-describedby="helper-text-explanation" 
+                                   
                                    class="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 
                                    focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
                                    dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
