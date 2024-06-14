a4fe8ce60794e3ab298f24bba94a1ac366b1050f
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2024-06-14 19:34:16 +0200

    Did a thing + Martijn tried to be whimsical again

--- Svelte/my-app/src/routes/register/+page.svelte
+++ Svelte/my-app/src/routes/register/+page.svelte
@@ -29,7 +29,7 @@
 
     function validatePassword(password: string): boolean {
         // Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!#%*?&]{8,}$/;
         return passwordRegex.test(password);
     }
 
